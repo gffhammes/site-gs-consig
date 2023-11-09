23dba9a2c8a65e4054fc50599092671ab0d96349
Guilherme Hammes <gffhammes@gmail.com>
2023-11-09 20:05:26 -0300

    feat: remove unecessary code

--- src/HomePage/Banner.tsx
+++ src/HomePage/Banner.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Box, Typography } from "@mui/material";
 import { IHomeBanner } from "../types/strapi";
 
@@ -8,14 +6,11 @@ export interface IBannerProps {
 }
 
 export const Banner = ({ banners }: IBannerProps) => {
-  console.log(banners);
-
   return (
     <Box
       sx={{
         height: "100%",
         width: "100%",
-        // backgroundColor: "red",
         backgroundImage: `url(${banners[0].foto.data.attributes.formats.large.url})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
