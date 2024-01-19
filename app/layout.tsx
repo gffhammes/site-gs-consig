45563b1cc39250fa8afb0e8e912a3146674bc2b1
Guilherme Hammes <gffhammes@gmail.com>
2024-01-19 16:53:24 -0300

    feat: add analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/src/common/Header/Header";
 import { SplashScreen } from "@/src/common/SplashScreen";
 import { Footer } from "@/src/common/Footer/Footer";
 import { WhatsAppFloatingButton } from "@/src/common/WhatsAppFloatingButton";
+import Script from "next/script";
 
 const nunito = Nunito_Sans({ subsets: ["latin"] });
 
@@ -28,6 +29,22 @@ export default function RootLayout({
           name="google-site-verification"
           content="MiwgkR2Wdy0GS0qpeZE11KUmGPhWXH5EFwlXb8L4kxg"
         />
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload" id="google-analytics">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
       </head>
 
       <body className={nunito.className}>
