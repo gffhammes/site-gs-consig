a99f2e5d800a6799f6f0e654d7f442a2c1bb557f
Guilherme Hammes <gffhammes@gmail.com>
2024-08-29 20:03:53 -0300

    feat: add fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { SplashScreen } from "@/src/common/SplashScreen";
 import { Footer } from "@/src/common/Footer/Footer";
 import { WhatsAppFloatingButton } from "@/src/common/WhatsAppFloatingButton";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const roboto = Roboto({ subsets: ["latin"], weight: "500" });
 
@@ -44,6 +45,29 @@ export default function RootLayout({
 
           <Footer />
         </ThemeRegistry>
+
+        <Script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '538397215325977');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=538397215325977&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
 
       <GoogleAnalytics gaId="G-VCNV518NYK" />
